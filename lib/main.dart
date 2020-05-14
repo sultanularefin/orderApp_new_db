6b71e9afddf6d7113d3b7a374bb7b50acab37a0a
unknown <mhmdarefin@gmail.com>
2020-05-14 16:50:42 +0600

    commit before pull

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'src/screens/foodGallery/foodgallery2.dart';
 
-import 'src/welcomePage.dart';
+//import 'src/welcomePage.dart';
 
 void main() => runApp(MyApp());
 
