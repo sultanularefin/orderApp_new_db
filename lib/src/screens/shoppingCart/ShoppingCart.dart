dbc421bd1c6c71f1f3d01f9a96b4e458c2a5e6a7
unknown <mhmdarefin@gmail.com>
2020-05-28 09:23:34 +0600

    before implementing user Input animation

--- lib/src/screens/shoppingCart/ShoppingCart.dart
+++ lib/src/screens/shoppingCart/ShoppingCart.dart
@@ -433,7 +433,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
 //                                                animatedUnObscuredTextInputContainer(),
                                                 child: oneOrder.deliveryTypeIndex ==1 ?
                                                 animatedUnObscuredTextInputContainer()
-                                                :oneOrder.deliveryTypeIndex ==2?
+                                                    :oneOrder.deliveryTypeIndex ==2?
                                                 animatedUnObscuredTextInputContainer():
                                                 animatedObscuredTextInputContainer(),
 
@@ -610,85 +610,199 @@ class _ShoppingCartState extends State<ShoppingCart> {
         initialData: shoppingCartbloc.getCurrentOrderType,
 
         builder: (context, snapshot)
-    {
-      if (!snapshot.hasData) {
-        print('!snapshot.hasData');
+        {
+          if (!snapshot.hasData) {
+            print('!snapshot.hasData');
 //        return Center(child: new LinearProgressIndicator());
-        return Container(child: Text('Null'));
-      }
-      else {
-        List<OrderTypeSingleSelect> allOrderTypesSingleSelect = snapshot.data;
+            return Container(child: Text('Null'));
+          }
+          else {
+            List<OrderTypeSingleSelect> allOrderTypesSingleSelect = snapshot.data;
 
 //            List<OrderTypeSingleSelect> orderTypes = shoppingCartBloc.getCurrentOrderType;
 
-        print('orderTypes: $allOrderTypesSingleSelect');
-        OrderTypeSingleSelect selectedOne = allOrderTypesSingleSelect
-            .firstWhere((oneOrderType) => oneOrderType.isSelected == true);
-        _currentOrderTypeIndex = selectedOne.index;
-
-        return Container(
-          width: displayWidth(context) / 1.1,
-          height: displayHeight(context) / 20,
-          color: Color(0xffffffff),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
+            print('orderTypes: $allOrderTypesSingleSelect');
+            OrderTypeSingleSelect selectedOne = allOrderTypesSingleSelect
+                .firstWhere((oneOrderType) => oneOrderType.isSelected == true);
+            _currentOrderTypeIndex = selectedOne.index;
 
 
-              Container(
-                width: displayWidth(context) /
-                    1.5,
-                height: displayHeight(
-                    context) / 20,
-                color: Color(0xffffffff),
+            String orderTypeName = selectedOne.orderType;
+            String orderIconName = selectedOne.orderIconName;
+            String borderColor = selectedOne.borderColor;
+            const Color OrderTypeIconColor=Color(0xff070707);
 
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment
-                        .start
-                    ,
-                    crossAxisAlignment: CrossAxisAlignment
-                        .center,
-                    children: <Widget>[
 
-                      Container(
-                        margin: EdgeInsets
-                            .fromLTRB(
-                            20, 0, 10, 0),
-                        alignment: Alignment
+
+
+            return Container(
+              width: displayWidth(context) / 1.1,
+              height: displayHeight(context) / 12,
+              color: Color(0xffffffff),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+
+
+                  Container(
+                    width: displayWidth(context) /
+                        1.5,
+                    height: displayHeight(
+                        context) / 20,
+                    color: Color(0xffffffff),
+
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment
+                            .start
+                        ,
+                        crossAxisAlignment: CrossAxisAlignment
                             .center,
-                        child: Text(
-                            'Choose Order Type',
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight
-                                  .normal,
+                        children: <Widget>[
+
+                          Container(
+                            margin: EdgeInsets
+                                .fromLTRB(
+                                20, 0, 10, 0),
+                            alignment: Alignment
+                                .center,
+                            child: Text(
+                                'Choose Order Type',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight
+                                      .normal,
 //                                                        fontFamily: 'GreatVibes-Regular',
 
 //                    fontStyle: FontStyle.italic,
-                              color: Color(
-                                  0xff000000),
-                            )
+                                  color: Color(
+                                      0xff000000),
+                                )
+                            ),
+                          ),
+
+                          CustomPaint(
+                            size: Size(0, 19),
+                            painter: LongPainterForanimatedWidgetShowSelectedOrderType(
+                                context),
+                          ),
+
+                        ]
+                    ),
+
+                  ),
+                  // THE ABOVE PART DEALS WITH LINES AND TEXT,
+                  // BELOW PART HANDLES RAISED BUTTON WITH SELECTED DELIVERY TYPE ICON:
+
+                  Container(
+
+                    width: 100,
+                    height: displayHeight(context) /10,
+//                    alignment: Alignment.center,
+//                    margin: EdgeInsets.fromLTRB(5, 0, 3, 0),
+                    child:
+                    OutlineButton(
+                      color: Color(0xff000000),
+
+//          elevation: 2.5,
+                      // RoundedRectangleBorder
+//          shape: CircleBorder(
+                      shape: RoundedRectangleBorder(
+//          borderRadius: BorderRadius.circular(15.0),
+                        side: BorderSide(
+                          color:Color(0xff000000),
+                          style: BorderStyle.solid,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+
+                      child:Container(
+//                        alignment: Alignment.topCenter,
+                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment
+                              .start
+                          ,
+//                          crossAxisAlignment: CrossAxisAlignment
+//                              .center,
+//                          AA
+                          children: <Widget>[
+
+                            new Container(
+
+//                                width: displayWidth(context) * 0.09,
+//                                height: displayWidth(context) * 0.11,
+
+                              width:  90,
+                              height: displayHeight(context) /15,
+//                decoration: new BoxDecoration(
+//                  color: Colors.orange,
+//                  shape: BoxShape.circle,
+//                ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+//                    color: Colors.black,
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0,
+
+                                ),
+                                shape: BoxShape.circle,
+//                    borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
+//                padding:EdgeInsets.symmetric(vertical: 7,horizontal: 0),
+
+
+                              child: Icon(
+                                getIconForName(orderTypeName),
+                                color: Colors.black,
+                                size: displayHeight(context) /24,
+
+                              ),
+                            ),
+
+                            Container(
+
+                              alignment: Alignment.center,
+                              child: Text(
+                                orderTypeName, style:
+                              TextStyle(
+                                  color:Colors.red,
+
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
+                      onPressed: () {
 
-                      CustomPaint(
-                        size: Size(0, 19),
-                        painter: LongPainterForanimatedWidgetShowSelectedOrderType(
-                            context),
-                      ),
+                        //final shoppingCartBloc = BlocProvider.of<ShoppingCartBloc>(context);
+//              final locationBloc = BlocProvider.of<>(context);
+                        //shoppingCartBloc.setOrderTypeSingleSelectOptionForOrder(x,index,_currentOrderTypeIndex);
 
-                    ]
-                ),
+                        setState(() {
+                          showFullOrderType = !showFullOrderType;
+                        });
 
+
+                      },
+                    ),
+                    // : Container for 2nd argument of ternary condition ends here.
+
+                  )
+
+
+
+                  //ZZZZ
+
+
+                ],
               ),
-
-
-            ],
-          ),
-        );
-      }
-    }
+            );
+          }
+        }
     );
   }
 
@@ -719,9 +833,9 @@ class _ShoppingCartState extends State<ShoppingCart> {
 //        child: _buildShoppingCartInputFields()
 //    ),
 
-   print('at animated Obscured Text Input Container');
+    print('at animated Obscured Text Input Container');
     return
-    AbsorbPointer(
+      AbsorbPointer(
         child: Opacity(
           opacity:0.8,
           child: Container(
@@ -975,7 +1089,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
                                     border: InputBorder.none,
                                     hintText: 'Enter delivery location',
                                     hintStyle: TextStyle(
-                                        color: Color(0xffFC0000), fontSize: 16),
+                                        color: Color(0xffFC0000), fontSize: 17),
 
 //                                      currentUser
 //                                        labelText: 'Search about meal.'
@@ -1119,7 +1233,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
                                     border: InputBorder.none,
                                     hintText: 'Enter House/Flat address/number',
                                     hintStyle: TextStyle(
-                                        color: Color(0xffFC0000), fontSize: 16),
+                                        color: Color(0xffFC0000), fontSize: 17),
 
 //                                        labelText: 'Search about meal.'
                                   ),
@@ -1262,7 +1376,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
                                     border: InputBorder.none,
                                     hintText: 'Enter phone / telephone number',
                                     hintStyle: TextStyle(
-                                        color: Color(0xffFC0000), fontSize: 16),
+                                        color: Color(0xffFC0000), fontSize: 17),
 
 //                                        labelText: 'Search about meal.'
                                   ),
@@ -1407,7 +1521,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
                                     border: InputBorder.none,
                                     hintText: 'Enter reach out time',
                                     hintStyle: TextStyle(
-                                        color: Color(0xffFC0000), fontSize: 16),
+                                        color: Color(0xffFC0000), fontSize: 17),
 
 //                                        labelText: 'Search about meal.'
                                   ),
