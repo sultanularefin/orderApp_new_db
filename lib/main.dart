a54f2c6524096cac7d38735811f6c168bb615a18
sultanularefin <mhmdarefin@gmail.com>
2020-09-24 16:04:58 +0600

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -33,19 +33,8 @@ class MyApp extends StatelessWidget {
             title: 'Flutter Demo',
             // commented for Tablet testing on april 25.
             theme: ThemeData(
-              // This is the theme of your application.
-              //
-              // Try running your application with "flutter run". You'll see the
-              // application has a blue toolbar. Then, without quitting the app, try
-              // changing the primarySwatch below to Colors.green and then invoke
-              // "hot reload" (press "r" in the console where you ran "flutter run",
-              // or simply save your changes to "hot reload" in a Flutter IDE).
-              // Notice that the counter didn't reset back to zero; the application
-              // is not restarted.
+
               primarySwatch: Colors.blue,
-              // This makes the visual density adapt to the platform that you run
-              // the app on. For desktop platforms, the controls will be smaller and
-              // closer together (more dense) than on mobile platforms.
               visualDensity: VisualDensity.adaptivePlatformDensity,
               appBarTheme:AppBarTheme(
 
@@ -53,27 +42,14 @@ class MyApp extends StatelessWidget {
 
                 elevation:0,
 
-//                shadowColor:Colors.lightBlueAccent,
-//                brightness:,
-//                iconTheme,
-//                actionsIconTheme,
-//                textTheme,
-//                centerTitle,
-
               )
-
-//              appBarTheme.shadowColor:
 
             ),
             debugShowCheckedModeBanner: false,
-//      home: WelcomePage(),
             home:WelcomePage(),
 
-
-//      home: FoodGallery(),
           ),
-        // ),
-//        ),
+
       );
   }
 }
